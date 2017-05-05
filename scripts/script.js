865d7a4a3030019277a0be9106d1d74c2a6877ca
Jordan Cotton <Jordancotton55@gmail.com>
2017-05-05 04:52:05 +0300

    Update API logic, update JS to ES2015 standard

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,41 @@
-$(function () {  //Call our quote function once upon user landing on-page
+$(() => {  //Call our quote function once upon user landing on-page
   generateQuote();
 });
 
 /* When user clicks on the 'Tweet' button, we add the currently displayed quote data
 from our in-script variables */
-$('#tweet').click(function() {
+$('#tweet').click(() => {
   $(this).attr("href", "https://twitter.com/intent/tweet?text=" + tweet_content);
 });
 
-$('#quote_button').on("click", function() { 
+$('#quote-button').on("click", () => { 
   generateQuote();
 });
 
-var tweet_content = "";
+let tweet_content = "";
 
-function generateQuote () {
-  $.ajax({ dataType: 'jsonp',
-    url: "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", 
-  success: function callback (val)   {
-    /* $("#quote").html(val.quoteText);
+let generateQuote = () => {
+  $.ajax({ headers: {
+    "X-Mashape-Key": '8MtOjsB8wzmshjFt2sWPsjjcpyhdp1yIpkzjsnu3M83ysYSI9h',
+    Accept: 'application/json',
+    "Content-Type": 'application/x-www-form-urlencoded'
+  },
+  url: "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous&count=1", 
+  success: (res) => {
+    let val = JSON.parse(res);
 
-    if (!val.quoteAuthor) { 
+   $("#quote").html(val.quote);
+
+    if (!val.author) { 
       $("#author").html("- Anonymous");
     }
     else {
-    $("#author").html("- " + val.quoteAuthor);
+    $("#author").html("- " + val.author);
     }
 
-    tweet_content = '"' + val.quoteText + '"' + " -" + val.quoteAuthor; */
-
-    console.log(val);
-  },
+    //Prepopulate the content of the tweet if the user decides to tweet it
+    tweet_content = '"' + val.quote + '"' + " -" + val.author; 
+  }
    });
 
    
