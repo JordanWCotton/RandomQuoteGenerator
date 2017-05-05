d7e83052c76c367adeda47cb970f8dea186af4db
Jordan Cotton <Jordancotton55@gmail.com>
2017-05-05 03:31:12 +0300

    API call now made to HTTPS

--- scripts/script.js
+++ scripts/script.js
@@ -15,7 +15,7 @@ $('#quote_button').on("click", function() {
 var tweet_content = "";
 
 function generateQuote () {
-  $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(val)   {
+  $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(val)   {
     $("#quote").html(val.quoteText);
 
     if (!val.quoteAuthor) { 
